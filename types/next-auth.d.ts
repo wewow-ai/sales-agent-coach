a354dac3a659b061312dd95648e7c8ae9913b321
oqyshi <artemovanvar2@gmail.com>
2025-05-20 09:12:12 +0200

    feat: add user id to session

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,6 +3,7 @@ import { Role } from '@prisma/client'
 declare module 'next-auth' {
   interface Session {
     user: {
+      id:? string
       name?: string | null
       email?: string | null
       image?: string | null
